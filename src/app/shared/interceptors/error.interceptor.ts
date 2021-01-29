3f9b3c0f443b310f8af39ce274aef0b6fb2fb53b
tbone7243 <tbone7243@gmail.com>
2021-01-29 15:00:37 -0500

    Finished the error handling component.

--- src/app/shared/interceptors/error.interceptor.ts
+++ src/app/shared/interceptors/error.interceptor.ts
@@ -2,17 +2,33 @@ import { Injectable } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ErrorComponent } from 'src/app/components/shared/error/error.component';
+import { MatDialog } from '@angular/material/dialog';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(
+    private dialog: MatDialog
+  ) { }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = "Something went wrong, an unknown error has occurred!"
+        if(error.error.message) {
+          errorMessage = error.error.message;
+        }
+        this.dialog.open(ErrorComponent, { data: { message: errorMessage}});
+        //  alert(error.error.error.message);
+        return throwError(error);
+      })
+    );
   }
 }
