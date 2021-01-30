99ae14637f0ec3c245510683993019bca7e60418
Thomas E. Kremer <68854175+tkremer72@users.noreply.github.com>
2021-01-30 08:59:59 -0500

    Update verify-auth.js

--- backend/middleware/verify-auth.js
+++ backend/middleware/verify-auth.js
@@ -5,7 +5,7 @@ try {
   const token = req.headers.authorization.split(" ")[1];
   const decodedToken = jwt.verify(
   token,
-  'this_is_the_secret_key_this_should_be_very_long_this_should_never_be_given_to_anyone_ever_the_developer_is_the_only_person_who_should_ever_know_or_be_in_posession_of_this_secret_key'
+  process.env.JWT_KEY  
   );
     req.userData = { user_email: decodedToken.user_email, userId: decodedToken.userId, is_admin: decodedToken.is_admin}
   next()
